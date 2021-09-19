5a61ba504602013e43da25890734cb41b9d0fe70
Maciej Kaczanowski <maciej.emil.kaczanowski@gmail.com>
2021-09-19 15:56:58 +0200

    + Add [CORE-1] Base screen, and state handling

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// TODO add material-ui
+
+enum Screens {
+    MainMenu = 'MainMenu',
+    Game = 'Game',
+    Settings = 'Settings',
+    Credits = 'Credits'
+}
+
+interface ScreenSelectorProps {
+    selectedScree: Screens;
+    setSelectedScreen: (screen: Screens) => void;
+}
+
+const ScreenSelector = (props: ScreenSelectorProps) => {
+    if (props.selectedScree === Screens.Settings) {
+        return (<div>Settings</div>)
+    }
+    if (props.selectedScree === Screens.Game) {
+        return (<div>Game</div>)
+    }
+    if (props.selectedScree === Screens.Credits) {
+        return (<div>Credits</div>)
+    }
+
+    return <div>MainMenu</div>
+
+}
+const App = () => {
+    const [currentScreen, setCurrentScreen] = useState<Screens>(Screens.MainMenu)
+    return (
+        <div className="App">
+            <header className="App-header">
+                <ScreenSelector
+                    selectedScree={currentScreen}
+                    setSelectedScreen={(screen) => setCurrentScreen(screen)}
+                />
+            </header>
+        </div>
+    );
 }
 
 export default App;
