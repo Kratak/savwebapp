b34247d3a421747618959d7aba990cc8cdd522d5
Maciej Kaczanowski <maciej.emil.kaczanowski@gmail.com>
2022-04-21 10:12:02 +0200

    + Add [SAV-102] main menu resume/load screen is available only if there ar saved games

--- src/screens/mainMenu/useMainMenu.ts
+++ src/screens/mainMenu/useMainMenu.ts
@@ -1,13 +1,17 @@
-import { useState } from 'react';
-import { Screens, ScreenSelectorProps } from '../types';
-import { useStyles } from './styles';
+import { useEffect, useState } from 'react';
+
 import { useInGameScreenPush } from '../../helpers/useInGameScreenPush';
+import { useGameSaves } from '../../helpers';
+
+import { useStyles } from './styles';
+import { Screens, ScreenSelectorProps } from '../types';
 
 export const useMainMenu = (props: ScreenSelectorProps) => {
     const styles = useStyles();
+    const { getSaveSlot } = useGameSaves();
     const { screenHandlers } = useInGameScreenPush(props);
     const [resumeAvailable, setResumeAvailable] = useState<boolean>(false);
-    const [loadAvailable, setLoadAvailable] = useState<boolean>(true);
+    const [loadAvailable, setLoadAvailable] = useState<boolean>(false);
 
     const handleResumeGame = () => {
         console.log('handleResumeGame');
@@ -28,6 +32,17 @@ export const useMainMenu = (props: ScreenSelectorProps) => {
         console.log('handleResumeGame');
         screenHandlers.gotToSelectedScreen(Screens.GameFaber);
     };
+
+    useEffect(() => {
+        getSaveSlot()
+            .then(saveData => {
+                if (saveData.length > 0) {
+                    setLoadAvailable(true);
+                    setResumeAvailable(true);
+                }
+            })
+            .catch(e => console.log('Issue during load saves', e));
+    }, []);
 
     return {
         styles,
